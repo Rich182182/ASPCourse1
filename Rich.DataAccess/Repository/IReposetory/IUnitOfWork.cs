4b196e11c13e34fd60cb9a9e0af115f6b7be0a1d
Rich182 <zhenu1276@gmail.com>
2025-01-10 16:44:25 +0200

    Areas, Repositories

---
+++ Rich.DataAccess/Repository/IReposetory/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rich.DataAccess.Repository.IReposetory
+{
+    public interface IUnitOfWork
+    {
+        ICategoryRepository Category{ get; }
+        void Save();
+    }
+}
